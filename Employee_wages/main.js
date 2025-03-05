f3f8d719af64a4297a1512f06ad184878e47521a
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-03-05 12:39:42 +0530

    Unaiz- add . employee daily wage calculation

--- Employee_wages/main.js
+++ Employee_wages/main.js
@@ -1,9 +1,25 @@
-const EMPLOYEE_PRESENT = 1;
 
-let attendance = Math.floor(Math.random() * 2);
+const WAGE_PER_HOUR = 20;
+const FULL_TIME_HOURS = 8;
+const PART_TIME_HOURS = 4;
 
-if (attendance === EMPLOYEE_PRESENT) {
-    console.log("Employee is Present");
-} else {
-    console.log("Employee is Absent");
+let workType = Math.floor(Math.random() * 3);
+let workHours = 0;
+
+switch (workType) {
+    case 1:
+        workHours = PART_TIME_HOURS;
+        console.log("Employee worked Part Time");
+        break;
+    case 2:
+        workHours = FULL_TIME_HOURS;
+        console.log("Employee worked Full Time");
+        break;
+    default:
+        workHours = 0;
+        console.log("Employee did not work");
+        break;
 }
+
+let dailyWage = workHours * WAGE_PER_HOUR;
+console.log("Daily Wage: $" + dailyWage);
