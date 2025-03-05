a1409dfaaee2a7448a5a3c6af94f6494e40b73d6
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-03-05 12:56:06 +0530

    Unaiz - add Refactored code to use function for work hours

--- Employee_wages/main.js
+++ Employee_wages/main.js
@@ -1,24 +1,22 @@
-
 const WAGE_PER_HOUR = 20;
 const FULL_TIME_HOURS = 8;
 const PART_TIME_HOURS = 4;
 
 let workType = Math.floor(Math.random() * 3);
-let workHours = 0;
 
 switch (workType) {
-    case 1:
-        workHours = PART_TIME_HOURS;
-        console.log("Employee worked Part Time");
-        break;
-    case 2:
-        workHours = FULL_TIME_HOURS;
-        console.log("Employee worked Full Time");
-        break;
-    default:
-        workHours = 0;
-        console.log("Employee did not work");
-        break;
+  case 1:
+    workHours = PART_TIME_HOURS;
+    console.log("Employee worked Part Time");
+    break;
+  case 2:
+    workHours = FULL_TIME_HOURS;
+    console.log("Employee worked Full Time");
+    break;
+  default:
+    workHours = 0;
+    console.log("Employee did not work");
+    break;
 }
 
 let dailyWage = workHours * WAGE_PER_HOUR;
